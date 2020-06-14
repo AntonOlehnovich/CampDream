77efe31bd80ee8fd523b2b8514879616412667a5
AntonOlehnovich <genetato9999@mail.ru>
2020-06-14 16:22:52 +0300

    Разработал анимацию нажатия на ссылки в шапке сайта в файле AnimatedSlidingMenu.js

---
+++ js/AnimatedSlidingMenu.js
@@ -0,0 +1,11 @@
+var marker = document.querySelector('#marker');
+var item = document.querySelectorAll('header a');
+function indicator(e) {
+    marker.style.left = e.offsetLeft+"px";
+    marker.style.width = e.offsetWidth+"px";
+}
+item.forEach(Link =>{
+    Link.addEventListener('click', (e)=>{
+        indicator(e.target);
+    })
+})
